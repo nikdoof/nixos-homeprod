4e4cae499fabd006a76ecadc9e9e52d26d22ed66
Andrew Williams <andy@tensixtyone.com>
2026-02-05 22:10:53 +0000

    Add missing file

---
+++ modules/podman.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  virtualisation = {
+    containers.enable = true;
+    oci-containers.backend = "podman";
+    podman = {
+      enable = true;
+      autoPrune.enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    dive
+    podman-tui
+  ];
+}
