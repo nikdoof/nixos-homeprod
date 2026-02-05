c1811d6ec2d875e4af4861754925f0a60d8ff4b1
Andrew Williams <andy@tensixtyone.com>
2026-02-05 22:35:48 +0000

    Add agenix

--- flake.nix
+++ flake.nix
@@ -4,16 +4,28 @@
   inputs = {
     # NixOS official package source, using the nixos-25.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    agenix.url = "github:ryantm/agenix";
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
     {
-
-      nixosConfigurations.svc-01 = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./hosts/svc-01/configuration.nix
-        ];
+      self,
+      nixpkgs,
+      agenix,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
+          modules = [
+            agenix.nixosModules.default
+          ];
+        };
+        svc-01 = default {
+          modules = [
+            ./hosts/svc-01/configuration.nix
+          ];
+        };
       };
     };
 }
---
+++ secrets.nix
@@ -0,0 +1,10 @@
+let
+  nikdoof = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHWO2qwHaPaQs46na4Aa6gMkw5QqRHUMGQphtgAcDJOw";
+  users = [ nikdoof ];
+
+  svc-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPHbFY5oPMjAinz46BD8qHTuMgjymS3Vo+57h+iKKWu";
+  systems = [ svc-01 ];
+in
+{
+
+}
