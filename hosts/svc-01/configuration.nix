f0aece4888562168b6d87d5be12d64be386e712c
Andrew Williams <andy@tensixtyone.com>
2026-02-05 21:59:36 +0000

    Provide FQDN for the NAS

--- hosts/svc-01/configuration.nix
+++ hosts/svc-01/configuration.nix
@@ -35,7 +35,7 @@
 
   # Media
   fileSystems."/mnt/nas-03/media" = {
-    device = "nas-03:/mnt/media";
+    device = "nas-03.int.doofnet.uk:/mnt/media";
     fsType = "nfs";
     options = [ "rw" "noatime" "nfsvers=4" "proto=tcp" ];
   };
