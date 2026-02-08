380a069f587c85e35be22ab5f9ce86773095eb09
Andrew Williams <andy@tensixtyone.com>
2026-02-08 14:06:35 +0000

    Fix DNS config

--- hosts/svc-01/configuration.nix
+++ hosts/svc-01/configuration.nix
@@ -26,10 +26,16 @@
   # Networking
   networking.useDHCP = false;
   networking.hostName = "svc-01"; # Define your hostname.
+  networking.nameservers = [
+    "10.101.1.2"
+    "2001:8b0:bd9:101::2"
+    "10.101.1.3"
+    "2001:8b0:bd9:101::3"
+  ];
   networking.domain = "int.doofnet.uk";
   networking.search = [ "int.doofnet.uk" ];
   systemd.network.enable = true;
-  systemd.network.networks."10-lan" = {
+  systemd.network.systemd.network.networks."10-lan" = {
     matchConfig.Name = "eno1";
     networkConfig = {
       DHCP = "ipv4";
