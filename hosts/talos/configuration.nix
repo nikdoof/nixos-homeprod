b8a32ef245e231d4c49bfd1808caec130b309a19
Andrew Williams <andy@tensixtyone.com>
2026-02-12 00:06:37 +0000

    Add Hyprland for Talos

--- hosts/talos/configuration.nix
+++ hosts/talos/configuration.nix
@@ -30,4 +30,12 @@
     HandleLidSwitchExternalPower = "ignore";
     HandleLidSwitchDocked = "ignore";
   };
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+  };
+
 }
