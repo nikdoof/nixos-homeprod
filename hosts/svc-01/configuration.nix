6607c9020545f0c5fbaa36feafb566a48343d11f
Andrew Williams <andy@tensixtyone.com>
2026-02-14 22:12:36 +0000

    Oops, move the config back to where it needs to be

--- hosts/svc-01/configuration.nix
+++ hosts/svc-01/configuration.nix
@@ -132,6 +132,79 @@
     '';
   };
 
+  services.traefik = {
+    staticConfigOptions = {
+      api = true;
+
+      entryPoints = {
+        extweb = {
+          address = ":8080";
+          asDefault = false;
+          http.redirections.entrypoint = {
+            to = "extwebsecure";
+            scheme = "https";
+          };
+        };
+
+        extwebsecure = {
+          address = ":8443";
+          asDefault = false;
+          http.tls.certResolver = "letsencrypt";
+        };
+      };
+
+      dynamicConfigOptions = {
+
+        http.routers = {
+          api = {
+            rule = "Host(`traefik.svc.doofnet.uk`)";
+            service = "api@internal";
+          };
+        };
+
+        http.middlewares = {
+          auth-headers = {
+            headers = {
+              sslRedirect = true;
+              stsSeconds = 315360000;
+              browserXssFilter = true;
+              contentTypeNosniff = true;
+              forceSTSHeader = true;
+              sslHost = "doofnet.uk";
+              stsIncludeSubdomains = true;
+              stsPreload = true;
+              frameDeny = true;
+            };
+          };
+
+          # Redirects if not authenticated
+          oauth-auth-redirect = {
+            forwardAuth = {
+              address = "http://127.0.0.1:4180/oauth2/";
+              trustForwardHeader = true;
+              authResponseHeaders = [
+                "X-Auth-Request-Access-Token"
+                "Authorization"
+              ];
+            };
+          };
+
+          # Throws 401 without redirecting
+          oauth-auth-wo-redirect = {
+            forwardAuth = {
+              address = "http://127.0.0.1:4180/oauth2/auth";
+              trustForwardHeader = true;
+              authResponseHeaders = [
+                "X-Auth-Request-Access-Token"
+                "Authorization"
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
 
