baada944d50bd4a408fc3de52c6ca855f39396bc
Andrew Williams <andy@tensixtyone.com>
2026-02-17 15:57:48 +0000

    Disable InfluxDB output

--- hosts/svc-02/configuration.nix
+++ hosts/svc-02/configuration.nix
@@ -207,6 +207,7 @@
   services.unpoller = {
     enable = true;
     prometheus.http_listen = "127.0.0.1:9130";
+    influxdb.disable = true;
     unifi.controllers = [
       {
         url = "https://127.0.0.1:8443";
