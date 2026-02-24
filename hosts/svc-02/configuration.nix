8f782c613938516aa9cefb74406c9d8aa90ea6bd
Andrew Williams <andy@tensixtyone.com>
2026-02-24 21:34:42 +0000

    Loki config again

--- hosts/svc-02/configuration.nix
+++ hosts/svc-02/configuration.nix
@@ -354,7 +354,40 @@
     configuration = {
       server.http_listen_port = 3030;
       auth_enabled = false;
+
+      common = {
+        ring = {
+          instance_addr = "127.0.0.1";
+          kvstore = {
+            store = "inmemory";
+          };
+        };
+        replication_factor = 1;
+        path_prefix = "/srv/data/loki";
+      };
+
+      schema_config = {
+        configs = [
+          {
+            from = "2020-05-15";
+            store = "tsdb";
+            object_store = "filesystem";
+            schema = "v13";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+      };
+
+      storage_config = {
+        filesystem = {
+          directory = "/srv/data/loki/chunks";
+        };
+      };
     };
+
   };
 
   services.unpoller = {
