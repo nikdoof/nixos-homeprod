90bb79971bebdff2f8497f69001c79adf8630adc
Andrew Williams <andy@tensixtyone.com>
2026-02-25 21:51:56 +0000

    Fix NFS module

--- modules/nfs/default.nix
+++ modules/nfs/default.nix
@@ -6,6 +6,13 @@
 with lib;
 let
   cfg = config.doofnet.nfs;
+  server_hostname = "nas-03.int.doofnet.uk";
+  nfs_options = [
+    "rw"
+    "noatime"
+    "nfsvers=4"
+    "proto=tcp"
+  ];
 in
 {
   options.doofnet.nfs = {
@@ -16,39 +23,24 @@ in
 
   config = {
     # Media
-    fileSystems."/mnt/nas-03/media" = mkIf cfg.doofnet.nfs.media {
-      device = "nas-03.int.doofnet.uk:/mnt/media";
+    fileSystems."/mnt/nas-03/media" = mkIf cfg.media {
+      device = "${server_hostname}:/mnt/media";
       fsType = "nfs";
-      options = [
-        "rw"
-        "noatime"
-        "nfsvers=4"
-        "proto=tcp"
-      ];
+      options = nfs_options;
     };
 
     # Photos
-    fileSystems."/mnt/nas-03/photos" = mkIf cfg.doofnet.nfs.photos {
-      device = "nas-03.int.doofnet.uk:/mnt/tank02/photos";
+    fileSystems."/mnt/nas-03/photos" = mkIf cfg.photos {
+      device = "${server_hostname}:/mnt/tank02/photos";
       fsType = "nfs";
-      options = [
-        "rw"
-        "noatime"
-        "nfsvers=4"
-        "proto=tcp"
-      ];
+      options = nfs_options;
     };
 
     # Paperless
-    fileSystems."/mnt/nas-03/paperless" = mkIf cfg.doofnet.nfs.paperless {
-      device = "nas-03.int.doofnet.uk:/mnt/ssd-mirror/shares/paperless";
+    fileSystems."/mnt/nas-03/paperless" = mkIf cfg.paperless {
+      device = "${server_hostname}:/mnt/ssd-mirror/shares/paperless";
       fsType = "nfs";
-      options = [
-        "rw"
-        "noatime"
-        "nfsvers=4"
-        "proto=tcp"
-      ];
+      options = nfs_options;
     };
   };
 }
