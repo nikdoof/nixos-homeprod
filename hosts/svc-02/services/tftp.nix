1d5cedc47bbca6729d0e060964c01e7a446aad41
Andrew Williams <andy@tensixtyone.com>
2026-02-26 22:11:46 +0000

    Add TFTPD to SVC-02

--- hosts/svc-02/services/tftp.nix
+++ hosts/svc-02/services/tftp.nix
@@ -1,1 +1,14 @@
-{ ... }: { }
+{ lib, ... }:
+{
+  services.atftpd = {
+    enable = true;
+    root = "/srv/data/tftp";
+  };
+
+  networking.firewall.allowedUDPPorts = [ 69 ];
+
+  system.activationScripts.atftpd = lib.stringAfter [ "var" ] ''
+    if ! [ -f /srv/data/tftp ]; then mkdir -p "/srv/data/tftp"; fi
+    chmod u+rwX,g+rX,o+rX "/srv/data/tftp"
+  '';
+}
