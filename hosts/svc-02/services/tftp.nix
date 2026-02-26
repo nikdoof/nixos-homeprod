f900e9f1b93a8d9f1edea55364183fca9a06e1f7
Andrew Williams <andy@tensixtyone.com>
2026-02-26 22:53:28 +0000

    Switch to rs-tftp

--- hosts/svc-02/services/tftp.nix
+++ hosts/svc-02/services/tftp.nix
@@ -1,13 +1,20 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
-  services.tftpd = {
-    enable = true;
-    path = "/srv/data/tftp";
+
+  systemd.services.rs-tftpd = {
+    description = "tftp server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.rs-tftpd}/bin/tftpd -r -d /srv/data/tftp";
+      DynamicUser = true;
+    };
   };
 
   networking.firewall.allowedUDPPorts = [ 69 ];
 
-  system.activationScripts.atftpd = lib.stringAfter [ "var" ] ''
+  system.activationScripts.rs-tftpd = lib.stringAfter [ "var" ] ''
     if ! [ -f /srv/data/tftp ]; then mkdir -p "/srv/data/tftp"; fi
     chmod u+rwX,g+rX,o+rX "/srv/data/tftp"
   '';
