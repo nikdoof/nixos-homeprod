f63f7829f3c7955a275b953d9d01d6c71bb3030e
Andrew Williams <andy@tensixtyone.com>
2026-03-01 21:46:42 +0000

    Switch back to RPi for NS-01

--- hosts/ns-01/configuration.nix
+++ hosts/ns-01/configuration.nix
@@ -1,53 +1,15 @@
 {
   inputs,
-  config,
-  lib,
   ...
 }:
-let
-  mkMac =
-    seed:
-    let
-      hash = builtins.hashString "md5" seed;
-      c = off: builtins.substring off 2 hash;
-    in
-    "${builtins.substring 0 1 hash}2:${c 2}:${c 4}:${c 6}:${c 8}:${c 10}";
-  mac = mkMac config.networking.fqdn;
-in
 {
   imports = [
     # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../hardware/raspberry-pi-3.nix
     ../../modules/doofnet
     inputs.microvm.nixosModules.microvm
   ];
-
-  microvm = {
-    hypervisor = "qemu";
-    vcpu = 2;
-    mem = 1024;
-    interfaces = [
-      {
-        type = "tap";
-        tap.vhost = true;
-        id = "vm-${config.networking.hostName}";
-        inherit mac;
-      }
-    ];
-    shares = [
-      {
-        source = "/nix/store";
-        mountPoint = "/nix/.ro-store";
-        tag = "ro-store";
-        proto = "virtiofs";
-      }
-      {
-        tag = "persist";
-        source = "/srv/data/persist/microvms/${config.networking.hostName}";
-        mountPoint = "/persist";
-        proto = "virtiofs";
-      }
-    ];
-  };
 
   # Networking
   networking.useDHCP = false;
@@ -80,20 +42,6 @@ in
     enable = true;
     mode = "primary";
   };
-
-  # Persist host key to persistant fs
-  fileSystems."/persist".neededForBoot = lib.mkForce true;
-  services.openssh.hostKeys = [
-    {
-      path = "/persist/ssh_host_ed25519_key";
-      type = "ed25519";
-    }
-    {
-      path = "/persist/ssh_host_rsa_key";
-      type = "rsa";
-      bits = 4096;
-    }
-  ];
 
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
