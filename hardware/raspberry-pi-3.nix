e62d0389eaf37b5a7453b12a86c0a96808f52053
Andrew Williams <andy@tensixtyone.com>
2026-03-08 00:38:51 +0000

    Add Pi RTC config #20

--- hardware/raspberry-pi-3.nix
+++ hardware/raspberry-pi-3.nix
@@ -14,4 +14,21 @@
     "bcm2835_dma"
     "i2c_bcm2835"
   ];
+
+  # Configure the Pi's RTC
+  systemd.services.pi-rtc = {
+    unitConfig = {
+      Description = "Configure and read the RTC on the I2C bus";
+      Before = "basic.target";
+      Conflicts = "shutdown.target";
+      DefaultDependencies = "no";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/bin/sh -c \"modprobe i2c_bcm2835 && modprobe rtc_ds1307 && sleep 1 && echo ds1307 0x68 > /sys/class/i2c-dev/i2c-1/device/new_device && hwclock -s\"";
+      ExecStop = "hwclock -w";
+      RemainAfterExit = "yes";
+    };
+    wantedBy = [ "basic.target" ];
+  };
 }
