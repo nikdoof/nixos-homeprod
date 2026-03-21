c883d2397a0d6cdaae15f3ab76049f7a620f2169
Andrew Williams <andy@tensixtyone.com>
2026-03-21 22:30:25 +0000

    grafana: add globaltalk alert

--- hosts/svc-02/services/grafana.nix
+++ hosts/svc-02/services/grafana.nix
@@ -80,6 +80,91 @@ in
         }
       ];
 
+      alerting.rules.settings.groups = [
+        {
+          orgId = 1;
+          name = "AppleTalk";
+          folder = "Alerts";
+          interval = "5m";
+          rules = [
+            {
+              uid = "atalkd-net-mismatch";
+              title = "GlobalTalk network collision";
+              condition = "C";
+              for = "0s";
+              noDataState = "OK";
+              execErrState = "Error";
+              annotations.summary = "AppleTalk daemon on afp-01 is reporting a network collision.";
+              data = [
+                {
+                  refId = "A";
+                  datasourceUid = "loki";
+                  queryType = "instant";
+                  relativeTimeRange = {
+                    from = 300;
+                    to = 0;
+                  };
+                  model = {
+                    refId = "A";
+                    queryType = "instant";
+                    datasource = {
+                      type = "loki";
+                      uid = "loki";
+                    };
+                    expr = ''count_over_time({host="afp-01", unit="atalkd.service"} |~ `rtmp_packet (last|first)net mismatch (\d*)!=(\d*)` [5m])'';
+                  };
+                }
+                {
+                  refId = "B";
+                  datasourceUid = "__expr__";
+                  model = {
+                    refId = "B";
+                    type = "reduce";
+                    expression = "A";
+                    reducer = "last";
+                    datasource = {
+                      type = "__expr__";
+                      uid = "__expr__";
+                    };
+                  };
+                }
+                {
+                  refId = "C";
+                  datasourceUid = "__expr__";
+                  model = {
+                    refId = "C";
+                    type = "threshold";
+                    expression = "B";
+                    datasource = {
+                      type = "__expr__";
+                      uid = "__expr__";
+                    };
+                    conditions = [
+                      {
+                        evaluator = {
+                          params = [ 1 ];
+                          type = "gt";
+                        };
+                        operator = {
+                          type = "and";
+                        };
+                        query = {
+                          params = [ "B" ];
+                        };
+                        reducer = {
+                          type = "last";
+                        };
+                        type = "query";
+                      }
+                    ];
+                  };
+                }
+              ];
+            }
+          ];
+        }
+      ];
+
       datasources.settings.datasources = [
         {
           name = "prometheus";
@@ -91,6 +176,7 @@ in
         }
         {
           name = "loki";
+          uid = "loki";
           type = "loki";
           url = "https://loki.svc.doofnet.uk";
         }
