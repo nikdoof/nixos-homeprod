129608400947f19684ae9823f60f37da2d7248d0
Andrew Williams <andy@tensixtyone.com>
2026-03-21 17:11:43 +0000

    jrouter: open status port

--- hosts/svc-02/services/jrouter.nix
+++ hosts/svc-02/services/jrouter.nix
@@ -25,4 +25,6 @@ _: {
       peerlist_url = "https://gist.githubusercontent.com/nikdoof/976fc87b0d7abd3e5ec6f583e0b202db/raw/de73d44a0ca505253743ff5a418f000b79a8d129/gistfile1.txt";
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ 9459 ];
 }
