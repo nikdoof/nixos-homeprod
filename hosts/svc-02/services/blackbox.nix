aac19428048914c0aed62408b78015e0a89527e6
Andrew Williams <andy@tensixtyone.com>
2026-03-23 06:33:32 +0000

    blackbox: switch to alloy internal

--- hosts/svc-02/services/blackbox.nix
+++ hosts/svc-02/services/blackbox.nix
@@ -1,41 +1,40 @@
-{ pkgs, ... }:
-{
-  services.prometheus.exporters.blackbox = {
-    enable = true;
-    port = 9115;
-    listenAddress = "127.0.0.1";
-    configFile = pkgs.writeText "blackbox.yml" (
-      builtins.toJSON {
-        modules = {
-          https_2xx = {
-            prober = "http";
-            timeout = "10s";
-            http = {
-              valid_status_codes = [ ];
-              fail_if_not_ssl = true;
-              preferred_ip_protocol = "ip4";
-            };
-          };
-        };
-      }
-    );
-  };
-
+_: {
   # Alloy config
   environment.etc."alloy/conf.d/02-blackbox.alloy".text = ''
-    prometheus.scrape "blackbox_https" {
-      targets = [
-        {"__address__" = "localhost:9115", "__param_target" = "https://social.doofnet.uk",          "instance" = "social.doofnet.uk"},
-        {"__address__" = "localhost:9115", "__param_target" = "https://id.doofnet.uk",              "instance" = "id.doofnet.uk"},
-        {"__address__" = "localhost:9115", "__param_target" = "https://rss.doofnet.uk",             "instance" = "rss.doofnet.uk"},
-        {"__address__" = "localhost:9115", "__param_target" = "https://link.doofnet.uk",            "instance" = "link.doofnet.uk"},
-        {"__address__" = "localhost:9115", "__param_target" = "https://doofnet.uk",                 "instance" = "doofnet.uk"},
-        {"__address__" = "localhost:9115", "__param_target" = "https://nikdoof.com",                "instance" = "nikdoof.com"},
-      ]
-      forward_to   = [prometheus.remote_write.default.receiver]
-      job_name     = "blackbox"
-      metrics_path = "/probe"
-      params       = {"module" = ["https_2xx"]}
+      prometheus.exporter.blackbox "blackbox" {
+        config = "{ modules: { https_2xx: { prober: http, timeout: 10s, http: { valid_status_codes = [], fail_if_not_ssl = true, preferred_ip_protocol = ipv4 } } } }"
+
+        target {
+          name    = "social.doofnet.uk"
+          address = "https://social.doofnet.uk"
+          module  = "https_2xx"
+        }
+        target {
+          name    = "id.doofnet.uk"
+          address = "https://id.doofnet.uk"
+          module  = "https_2xx"
+        }
+        target {
+          name    = "rss.doofnet.uk"
+          address = "https://rss.doofnet.uk"
+          module  = "https_2xx"
+        }
+        target {
+          name    = "link.doofnet.uk"
+          address = "https://link.doofnet.uk"
+          module  = "https_2xx"
+        }
+        target {
+          name    = "doofnet.uk"
+          address = "https://doofnet.uk"
+          module  = "https_2xx"
+        }
+        target {
+          name    = "nikdoof.com"
+          address = "https://nikdoof.com"
+          module  = "https_2xx"
+        }
+      }
     }
   '';
 }
