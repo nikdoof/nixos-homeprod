bc320d9fadc8bdc8689d814057b26ff89d90eb55
Andrew Williams <andy@tensixtyone.com>
2026-04-02 00:08:03 +0100

    gw: add routes

--- hosts/gw/services/tailscale.nix
+++ hosts/gw/services/tailscale.nix
@@ -10,7 +10,7 @@
     openFirewall = false;
     useRoutingFeatures = "server";
     extraUpFlags = [
-      "--advertise-routes=10.0.0.0/8"
+      "--advertise-routes=10.0.0.0/8,2001:8b0:bd9::/48,fddd:d00f:dab0::/48"
       "--advertise-exit-node"
       "--login-server=https://hs.doofnet.uk"
     ];
