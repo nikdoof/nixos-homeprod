a53f4e71d9fba835198104c8e0e50aee29029c15
Andrew Williams <andy@tensixtyone.com>
2026-04-07 22:12:28 +0100

    radvd: complicate the radvd config more (#68)

--- hosts/gw/services/radvd.nix
+++ hosts/gw/services/radvd.nix
@@ -1,72 +1,74 @@
 { pkgs, ... }:
+let
+  configFile = pkgs.writeText "radvd.conf" ''
+    # VLAN 102 - Public
+    interface vlan-public {
+      AdvSendAdvert on;
+      AdvManagedFlag on;
+      AdvOtherConfigFlag on;
+      AdvDefaultPreference medium;
+
+      prefix 2001:8b0:bd9:102::/64 {
+        AdvOnLink on;
+        AdvAutonomous off;
+        AdvRouterAddr on;
+      };
+
+      RDNSS 2001:8b0:bd9:101::2 2001:8b0:bd9:101::3 {
+        AdvRDNSSLifetime 3600;
+      };
+
+      DNSSL pub.doofnet.uk {
+        AdvDNSSLLifetime 3600;
+      };
+    };
+
+    # VLAN 104 - Lab
+    interface vlan-lab {
+      AdvSendAdvert on;
+      AdvManagedFlag on;
+      AdvOtherConfigFlag on;
+      AdvDefaultPreference medium;
+
+      prefix 2001:8b0:bd9:104::/64 {
+        AdvOnLink on;
+        AdvAutonomous off;
+        AdvRouterAddr on;
+      };
+
+      prefix fddd:d00f:dab0:104::/64 {
+        AdvOnLink on;
+        AdvAutonomous on;
+      };
+
+      RDNSS 2001:8b0:bd9:101::2 2001:8b0:bd9:101::3 {
+        AdvRDNSSLifetime 3600;
+      };
+
+      DNSSL lab.doofnet.uk {
+        AdvDNSSLLifetime 3600;
+      };
+    };
+
+    # VLAN 106 - Hosted
+    interface vlan-hosted {
+      AdvSendAdvert on;
+      AdvManagedFlag off;
+      AdvOtherConfigFlag off;
+      AdvDefaultPreference medium;
+
+      prefix 2001:8b0:bd9:106::/64 {
+        AdvOnLink on;
+        AdvAutonomous on;
+        AdvRouterAddr on;
+      };
+    };
+  '';
+in
 {
   services.radvd = {
     enable = true;
-    # Static interfaces only — vlan-private is handled dynamically
-    config = ''
-      # VLAN 102 - Public
-      interface vlan-public {
-        AdvSendAdvert on;
-        AdvManagedFlag on;
-        AdvOtherConfigFlag on;
-        AdvDefaultPreference medium;
-
-        prefix 2001:8b0:bd9:102::/64 {
-          AdvOnLink on;
-          AdvAutonomous off;
-          AdvRouterAddr on;
-        };
-
-        RDNSS 2001:8b0:bd9:101::2 2001:8b0:bd9:101::3 {
-          AdvRDNSSLifetime 3600;
-        };
-
-        DNSSL pub.doofnet.uk {
-          AdvDNSSLLifetime 3600;
-        };
-      };
-
-      # VLAN 104 - Lab
-      interface vlan-lab {
-        AdvSendAdvert on;
-        AdvManagedFlag on;
-        AdvOtherConfigFlag on;
-        AdvDefaultPreference medium;
-
-        prefix 2001:8b0:bd9:104::/64 {
-          AdvOnLink on;
-          AdvAutonomous off;
-          AdvRouterAddr on;
-        };
-
-        prefix fddd:d00f:dab0:104::/64 {
-          AdvOnLink on;
-          AdvAutonomous on;
-        };
-
-        RDNSS 2001:8b0:bd9:101::2 2001:8b0:bd9:101::3 {
-          AdvRDNSSLifetime 3600;
-        };
-
-        DNSSL lab.doofnet.uk {
-          AdvDNSSLLifetime 3600;
-        };
-      };
-
-      # VLAN 106 - Hosted
-      interface vlan-hosted {
-        AdvSendAdvert on;
-        AdvManagedFlag off;
-        AdvOtherConfigFlag off;
-        AdvDefaultPreference medium;
-
-        prefix 2001:8b0:bd9:106::/64 {
-          AdvOnLink on;
-          AdvAutonomous on;
-          AdvRouterAddr on;
-        };
-      };
-    '';
+    config = "";
   };
 
   systemd.services.radvd-pd-init = {
@@ -116,11 +118,11 @@
     requires = [ "radvd-pd-init.service" ];
     serviceConfig = {
       ExecStartPre = pkgs.writeShellScript "radvd-merge-conf" ''
-        cat /etc/radvd.conf /run/radvd-dynamic.conf > /run/radvd-merged.conf
+        cat ${configFile} /run/radvd-dynamic.conf > /run/radvd-merged.conf
       '';
       ExecStart = pkgs.lib.mkForce "${pkgs.radvd}/sbin/radvd -n -C /run/radvd-merged.conf";
       ExecReload = pkgs.writeShellScript "radvd-reload" ''
-        cat /etc/radvd.conf /run/radvd-dynamic.conf > /run/radvd-merged.conf
+        cat ${configFile} /run/radvd-dynamic.conf > /run/radvd-merged.conf
         kill -HUP $MAINPID
       '';
     };
