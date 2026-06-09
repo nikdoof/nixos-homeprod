a150a4ad0cbe0160e25c3b3c05a25575231207d6
Andrew Williams <andy@tensixtyone.com>
2026-06-09 20:17:17 +0100

    Update hexgen

--- hosts/svc-01/services/hexgen.nix
+++ hosts/svc-01/services/hexgen.nix
@@ -7,7 +7,7 @@ _: {
         "traefik.http.services.hexgen.loadbalancer.server.port" = "5000";
         "traefik.http.routers.hexgen.entrypoints" = "websecure,extwebsecure";
       };
-      image = "ghcr.io/nikdoof/hexgen:1.4.1";
+      image = "ghcr.io/nikdoof/hexgen:1.4.3";
     };
   };
 }
