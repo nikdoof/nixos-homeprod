f7647ae5711ae7286ca7b2fa596b579107ec759c
Andrew Williams <andy@tensixtyone.com>
2026-06-17 22:04:28 +0100

    Fix DKIM keys

--- hosts/mx-01/services/rspamd.nix
+++ hosts/mx-01/services/rspamd.nix
@@ -1,8 +1,18 @@
-_: {
+_:
+let
+  selector = "658553b1d7df5d1bc6bdb2176d87b585a0ad54ce";
+in
+{
   services.rspamd = {
     enable = true;
 
     locals = {
+      "dkim_signing.conf".text = ''
+        allow_hdrhash_md5 = false;
+        sign_condition = "return true;";
+        selector = "${selector}";
+        path = "/persist/opendkim/keys/${selector}.private";
+      '';
       "greylisting.conf".text = "enabled = true;";
       "dmarc.conf".text = ''
         reporting_enabled = true;
