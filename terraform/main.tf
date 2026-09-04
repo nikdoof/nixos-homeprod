5c60785d2c60f99c3c478324e3221d86f1e85638
Andrew Williams <andy@tensixtyone.com>
2026-09-04 22:35:23 +0100

    Tidy up TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,34 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# IAM and instance roles
+#
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    effect  = "Allow"
+    sid     = "EC2"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# SSH keypairs
+#
+resource "aws_key_pair" "nikdoof_eu_west_1" {
+  provider   = aws.eu_west_1
+  key_name   = "nikdoof"
+  public_key = var.ssh_key
+}
+
+resource "aws_key_pair" "nikdoof_eu_west_2" {
+  provider   = aws.eu_west_2
+  key_name   = "nikdoof"
+  public_key = var.ssh_key
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
--- terraform/ns.tf
+++ terraform/ns.tf
@@ -41,17 +41,6 @@ data "aws_ami" "nixos_eu_west_2" {
 
 # IAM and instance roles
 #
-data "aws_iam_policy_document" "ec2_assume" {
-  statement {
-    effect  = "Allow"
-    sid     = "EC2"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
 
 resource "aws_iam_role" "ns" {
   name = "ns-role"
@@ -150,20 +139,6 @@ resource "aws_security_group" "ns_04" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-}
-
-# SSH keypairs
-#
-resource "aws_key_pair" "nikdoof_eu_west_1" {
-  provider   = aws.eu_west_1
-  key_name   = "nikdoof"
-  public_key = var.ssh_key
-}
-
-resource "aws_key_pair" "nikdoof_eu_west_2" {
-  provider   = aws.eu_west_2
-  key_name   = "nikdoof"
-  public_key = var.ssh_key
 }
 
 # Instances
